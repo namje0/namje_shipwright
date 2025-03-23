b04a8d4b4fd3a9a130badad010d2ba3aae689891
namje0 <danguyen.11902@gmail.com>
2025-03-22 19:35:50 -0600

    add get_ship_items, use handler, add stock_ship, add player respawn on ship swap

--- items/active/ship_licenses/shiplicense.lua
+++ items/active/ship_licenses/shiplicense.lua
@@ -1,3 +1,7 @@
+--[[function init()
+	world.spawnStagehand({1024, 1024}, "ship_handler")
+end]]
+
 function activate()
 	if player then
 		sb.logInfo("ship test: player activated license")
@@ -6,8 +10,12 @@ function activate()
 			return
 		end
 
+		local ship_type = config.getParameter("shipType")
+
 		animator.playSound("activate")
 		world.spawnStagehand({1024, 1024}, "ship_handler")
+		sb.logInfo(ship_type)
+		world.sendEntityMessage("ship_handler", "swap_ship", activeItem.ownerEntityId(), ship_type)
 		item.consume(1)
 	end
 end
--- stagehands/namje_ship/ship_handler.lua
+++ stagehands/namje_ship/ship_handler.lua
@@ -4,17 +4,80 @@ require "/scripts/util.lua"
 
 function init()
     sb.logInfo("stagehand created")
-    create_ship()
+    message.setHandler("swap_ship", swap_ship)
+    --create_ship()
+    --stagehand.die()
+end
+
+function swap_ship(_, _, ply, ship_type)
+    sb.logInfo("start ship swapping process")
+    local ship_items = get_ship_items()
+    create_ship(ply, ship_type)
+    stock_ship(ship_items)
+
+    local players = world.players()
+    for _, player in ipairs (players) do
+        world.sendEntityMessage(player, "fs_respawn")
+    end
+
     stagehand.die()
 end
 
-function create_ship()
+-- returns a list of every item stored in containers in the ship. Hopefully...
+--TODO: Fix radius, its too short range? Doesn't grab all items
+function get_ship_items()
+    local items = {}
+    local locker
+    local objects = world.objectQuery(entity.position(), 500)
+    for _, v in ipairs (objects) do
+        local is_container = world.containerSize(v) ~= nil
+        if is_container then
+            local shiplocker = string.find(world.entityName(v), "fuwallsafe")
+            if shiplocker then
+                locker = v
+            end
+            local container_items = world.containerItems(v)
+            for _, i in ipairs (container_items) do
+                table.insert(items, i)
+            end
+        end
+    end
+
+    --temp test
+    --[[
+    sb.logInfo("items in ship: " .. #items)
+    if locker then
+        for _, i in ipairs (items) do
+            world.containerAddItems(locker, i)
+        end
+    end
+    ]]
+    return items
+end
+
+function stock_ship(items)
+    local shiplocker
+    local objects = world.objectQuery(entity.position(), 500)
+    for _, v in ipairs (objects) do
+        shiplocker = string.find(world.entityName(v), "shiplocker")
+        if shiplocker then
+            for _, i in ipairs (items) do
+                world.containerAddItems(v, i)
+            end
+        end
+    end
+    if not shiplocker then
+        sb.logInfo("no shiplocker found")
+    end
+end
+
+function create_ship(ply, ship_type)
     sb.logInfo("create namjeship")
 
     local ship_config = root.assetJson("/frackinship/configs/ships.config")
 
     local ship_dungeon_id = config.getParameter("shipDungeonId", 10101)
-    local selected_ship = ship_config["namje_testship"]
+    local selected_ship = ship_config[ship_type]
     local replace_mode = {dungeon = "fu_byosblankquarter", size = {512, 512}}
 
     if selected_ship then
