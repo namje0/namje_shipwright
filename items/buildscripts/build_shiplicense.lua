a32f2992b3a97ea98350bfbfb7ff64b8d5abb064
namje0 <danguyen.11902@gmail.com>
2025-03-28 16:43:06 -0600

    add cargo hold size tooltip for ship licenses

--- items/buildscripts/build_shiplicense.lua
+++ items/buildscripts/build_shiplicense.lua
@@ -3,6 +3,7 @@ function build(directory, config, parameters, level, seed)
 
     config.tooltipFields.shipImage = config.shipImage
     config.tooltipFields.crewLabel = config.recCrewSize and "^orange;Recommended Crew Size:^reset; " .. config.recCrewSize or "^orange;Recommended Crew Size:^reset; 0"
+    config.tooltipFields.cargoLabel = config.cargoHoldSize and "^orange;Ship Cargo Hold Size:^reset; " .. config.cargoHoldSize or "^orange;Ship Cargo Hold Size:^reset; ^red;N/A^reset;"
 
     return config, parameters
 end
