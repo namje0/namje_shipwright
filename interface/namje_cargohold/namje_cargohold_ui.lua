98a823670566a6c6115d2d2764fe1bae93088fe5
namje0 <danguyen.11902@gmail.com>
2025-04-10 18:23:52 -0600

    complete first version of custom cargo hold

---
+++ interface/namje_cargohold/namje_cargohold_ui.lua
@@ -0,0 +1,65 @@
+local ini = init or function() end
+
+local items_per_row = 10
+local cargo_size
+
+--TODO: make world property for last time cargo hold was checked, compare the seconds and increment the timeToRot for every food item, replacing with rotten food if 0
+
+function init() ini()
+    widget.registerMemberCallback("cargoHoldScrollArea.itemList", "take_item", receive_item)
+    cargo_size = world.getProperty("namje_cargo_size", 0)
+end
+
+function update(dt)
+    local item = world.containerItemAt(pane.containerEntityId(), 0)
+    local cargo_hold = world.getProperty("namje_cargo_hold") or {}
+    if item and #cargo_hold < cargo_size then
+        world.sendEntityMessage(pane.containerEntityId(), "namje_cargohold_insert", item)
+    end
+    create_grid()
+end
+
+function create_grid()
+    widget.clearListItems("cargoHoldScrollArea.itemList")
+    
+    local num_items = 1
+    local rows = {}
+    local list = "cargoHoldScrollArea.itemList"
+    local cargo_hold = world.getProperty("namje_cargo_hold") or {}
+ 
+    local color = #cargo_hold >= cargo_size and "red" or #cargo_hold > cargo_size*.7 and "orange" or "white"
+    widget.setText("count", "CAPACITY ^" .. color .. ";" .. #cargo_hold .. "/" .. cargo_size .. "^reset;")
+
+    if #cargo_hold == 0 then
+        return
+    end
+
+    local num_rows = math.ceil(#cargo_hold/items_per_row)
+    for i = 1, num_rows do
+        rows[i] = string.format("%s.%s", list, widget.addListItem(list))
+    end
+
+    for i = 1, #rows do
+        for j = 1, items_per_row do
+            local item = cargo_hold[num_items] or nil
+            if item then
+                widget.setItemSlotItem(rows[i] .. ".slot" .. j, { name = item.name, count = item.count, parameters = item.parameters })
+                widget.setData(rows[i] .. ".slot" .. j, item)
+                num_items = num_items + 1
+            else
+                widget.setItemSlotItem(rows[i] .. ".slot" .. j, nil)
+            end
+        end
+    end
+end
+
+function receive_item(_, data)
+    if not data then
+        return
+    end
+
+    --local pos = mcontroller.position()
+    local pos = world.entityPosition(player.id())
+
+    world.sendEntityMessage(pane.containerEntityId(), "namje_receive_item", {data, pos})
+end
--- objects/namje_ship/cargo_holds/namje_cargohold.lua
+++ objects/namje_ship/cargo_holds/namje_cargohold.lua
@@ -1,25 +1,105 @@
-local slot_size
+local cargo_size
 local ini = init or function() end
 
 function init() ini()
-    local id = world.id()
-    if not string.find(id, "ClientShipWorld") then
+    if world.type() ~= "unknown" then
         object.smash(false)
     end
     
-    slot_size = world.getProperty("namje_cargo_size") or 0
+    cargo_size = world.getProperty("namje_cargo_size", 0)
     message.setHandler("namje_cargohold_insert", insert_item)
+    message.setHandler("namje_receive_item", receive_item)
 end
 
+function receive_item(_, _, args)
+    local item = args[1]
+    local pos = args[2]
+
+    if not item then
+        return
+    end
+
+    local cargo_hold = world.getProperty("namje_cargo_hold") or {}
+    if #cargo_hold <= 0 then
+        return
+    end
+
+    for k, v in pairs(cargo_hold) do
+        local cargo_item = v
+        if cargo_item and root.itemDescriptorsMatch(cargo_item, item, true) then
+            world.spawnItem(item, pos)
+            table.remove(cargo_hold, k)
+            world.setProperty("namje_cargo_hold", cargo_hold)
+            break
+        end
+    end
+end
 
 function insert_item(_, _, item)
     if not item then
         return
     end
-    world.containerTakeAll(entity.id())
-    sb.logInfo("namje // inserting item into cargohold: " .. item.name)
 
-    local cargo_hold = world.getProperty("namje_cargo_hold") or {}
-    table.insert(cargo_hold, item)
-    sb.logInfo("namje // cargo hold size: " .. #cargo_hold .. "/" .. slot_size)
+    local container_item = world.containerItemAt(entity.id(), 0)
+
+    if container_item and root.itemDescriptorsMatch(container_item, item, true) then
+        local cargo_hold = world.getProperty("namje_cargo_hold") or {}
+
+        if #cargo_hold >= cargo_size then
+            return
+        end
+
+        world.containerTakeAll(entity.id())
+        table.insert(cargo_hold, container_item)
+        world.setProperty("namje_cargo_hold", cargo_hold)
+    end
 end
+
+--[[
+function match_item(item, item2)
+    if not item or not item2 then
+        return
+    end
+
+    local para_1 = {item.parameters, item.count}
+    local para_2 = {item2.parameters, item2.count}
+
+    --checking the count first
+    if para_1[2] ~= para_2[2] then
+        return false
+    end
+
+    local count_1 = 0
+    local count_2 = 0
+
+    for _ in pairs(para_1[1]) do
+        count_1 = count_1 + 1
+    end
+
+    for _ in pairs(para_2[1]) do
+        count_2 = count_2 + 1
+    end
+
+    if count_1 ~= count_2 then
+        return false
+    end
+    
+    for k, v in pairs(para_1[1]) do
+        local item = para_2[1][k]
+        if type(item) == "table" and type(v) == "table" then
+            item = tostring(item)
+            v = tostring(v)
+            sb.logInfo(sb.print(item))
+            sb.logInfo(sb.print(v))
+        end
+        if item ~= v then
+            sb.logInfo("error, wrong value")
+            sb.logInfo(sb.print(item))
+            sb.logInfo(sb.print(v))
+            return false
+        end
+    end
+
+    return true
+end
+]]
