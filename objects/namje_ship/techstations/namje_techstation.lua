9c35b7528ffe5592cb192dcc3b0a9f5f61bc6e59
namje0 <danguyen.11902@gmail.com>
2025-07-15 23:20:47 -0600

    remove old test code

--- objects/namje_ship/techstations/namje_techstation.lua
+++ objects/namje_ship/techstations/namje_techstation.lua
@@ -1,16 +1,4 @@
 function init()
-    sb.logInfo("namje // init namje_techstation.lua")
-
-    message.setHandler("namje_set_ship_gravity", function(_, _, gravity)
-        --setGravity(gravity)
-        sb.logInfo("rahh")
-        --local template = world.template()
-        --sb.logInfo(sb.print(template))
-        --world.settemplate(temp)
-        --world.spawnStagehand({1024, 1024}, "namje_returnTemplate_stagehand")
-        --local template = world.sendEntityMessage("namje_returnTemplate_stagehand", "namje_returnTemplate")
-        --sb.logInfo(sb.print(template))
-    end)
 end
 --[[
 function onInteraction()
