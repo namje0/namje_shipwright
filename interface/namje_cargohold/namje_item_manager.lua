181d26ad445a248d88161d06b96ddda7c0b980af
namje0 <danguyen.11902@gmail.com>
2025-08-08 12:35:18 -0600

    refactor: optimize namje_item_manager:add_item() and namje_item_manager:stack_items

--- interface/namje_cargohold/namje_item_manager.lua
+++ interface/namje_cargohold/namje_item_manager.lua
@@ -41,6 +41,7 @@ function namje_item_manager:update_stats()
 end
 
 function namje_item_manager:populate_grid(filter)
+    self:update_capacity_text()
     self:give_excess()
 
     self.filter = filter
@@ -75,7 +76,7 @@ function namje_item_manager:populate_grid(filter)
             widget.setData(slot.name .. ".slot", slot.index)
 
             if item then
-                self:set_item(slot, item)
+                self:set_item(slot, item, true)
             end
         end
     end
@@ -89,71 +90,71 @@ function namje_item_manager:items_match(index, item)
     return root.itemDescriptorsMatch(self.cargo_content["slot_" .. index], item, true)
 end
 
-function namje_item_manager:stack_items(index, item)
-    if item and not self:items_match(index, item) then 
-        return 
-    end
-    local item_count = self.cargo_content["slot_" .. index].count
-
-    self.cargo_content["slot_" .. index].count = item_count + item.count
+function namje_item_manager:stack_items(index, new_count)
+    self.cargo_content["slot_" .. index].count = new_count
     local slot = self.slots[index]
-    widget.setItemSlotItem(slot.name .. ".slot", self.cargo_content["slot_" .. index])
+    if slot then
+        widget.setItemSlotItem(slot.name .. ".slot", self.cargo_content["slot_" .. index])
+    end
 end
 
 function namje_item_manager:add_item(item)
-    local new_item
-    if not item then
+    if not item or item.count <= 0 then
         return
     end
 
+    local max_stack = item.parameters.maxStack or root.itemConfig(item).config.maxStack or 1000
+
+    -- First pass: try to stack with existing items
     for i = 1, self.cargo_size do
-        local cargo_slot = self.cargo_content["slot_" .. i]
-        if self:items_match(i, item) then
-            local default_max_stack = 1000
-            local max_stack = item.parameters.maxStack or root.itemConfig(item).config.maxStack or default_max_stack
-            local slot_item = self:get_item(i)
-            if item.count + slot_item.count < max_stack then
-                self:stack_items(i, {name = item.name, parameters = item.parameters, count = item.count})
-                self:update_stats()
-                return
-            elseif slot_item.count ~= max_stack then
-                local remainder = (slot_item.count + item.count) % max_stack 
+        local slot_item = self.cargo_content["slot_" .. i]
+        if slot_item and self:items_match(i, item) then
+            local space_available = max_stack - slot_item.count
+            
+            if space_available > 0 then
+                local amount_to_add = math.min(item.count, space_available)
+                local new_count = slot_item.count + amount_to_add
+                
+                -- Call the new stack_items function with the final count
+                self:stack_items(i, new_count)
 
-                item.count = item.count - remainder
-                self:stack_items(i, {name = item.name, parameters = item.parameters, count = item.count})
-                self:update_stats()
-
-                if remainder <= 0 then
+                item.count = item.count - amount_to_add
+                if item.count <= 0 then
+                    self:update_stats()
                     return
                 end
-
-                new_item = {name = item.name, parameters = item.parameters, count = remainder}
-                self:add_item(new_item)
-                return
             end
         end
     end
+
+    -- Second pass: find an empty slot for the remaining item
     for i = 1, self.cargo_size do
-        local cargo_slot = self.cargo_content["slot_" .. i]
-        if not cargo_slot then
-            sb.logInfo("set into slot %s for %s", i, item)
-            self.cargo_content["slot_" .. i] = {name = item.name, parameters = item.parameters, count = item.count}
+        if not self.cargo_content["slot_" .. i] then
+            local item_to_add = {
+                name = item.name,
+                parameters = item.parameters,
+                count = item.count
+            }
+            
+            self.cargo_content["slot_" .. i] = item_to_add
             if self.slots[i] then
-                self:set_item(self.slots[i], {name = item.name, parameters = item.parameters, count = item.count})
-                widget.setItemSlotItem(self.slots[i].name .. ".slot", {name = item.name, parameters = item.parameters, count = item.count})
+                self:set_item(self.slots[i], item_to_add, false)
+                widget.setItemSlotItem(self.slots[i].name .. ".slot", item_to_add)
             end
             self:update_stats()
             return
         end
     end
-    return new_item
+    
+    self:update_stats()
+    return item
 end
 
 function namje_item_manager:get_item(index)
     return self.cargo_content["slot_" .. index] or nil
 end
 
-function namje_item_manager:set_item(slot, item)
+function namje_item_manager:set_item(slot, item, init)
     local prev_item = self:get_item(slot.index)
     if prev_item then
         
@@ -162,7 +163,9 @@ function namje_item_manager:set_item(slot, item)
     widget.setItemSlotItem(slot.name .. ".slot", item)
     self.cargo_content["slot_" .. slot.index] = item
 
-    self:update_stats()
+    if not init then
+        self:update_stats()
+    end
 end
 
 function namje_item_manager:lmb_slot(slot)
@@ -173,12 +176,12 @@ function namje_item_manager:lmb_slot(slot)
     local slot_item = self.cargo_content["slot_" .. slot.index]
 
     if slot_item and shift_held() then
-        self:set_item(slot, nil)
+        self:set_item(slot, nil, false)
         player.giveItem(slot_item)
         return
     end
 
-    self:set_item(slot, item or nil)
+    self:set_item(slot, item or nil, false)
     player.setSwapSlotItem(slot_item or nil)
 end
 
