93e0c4f420a014e8d28d0a42f5a7671dfa90c8c2
namje0 <danguyen.11902@gmail.com>
2025-08-25 01:28:35 -0600

    feat: add arg to get_ent_storage

--- scripts/namje_entStorageGrabber.lua
+++ scripts/namje_entStorageGrabber.lua
@@ -1,5 +1,9 @@
-function get_ent_storage()
-    return storage
+function get_ent_storage(value)
+    if #value > 0 then
+        return storage[value]
+    else
+        return storage
+    end
 end
 
 function set_ent_storage(values)
