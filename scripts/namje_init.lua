eca4da531cae68fe7eb94351327e9c81882a30e6
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2025-10-12 04:35:58 -0500

    namje_init.lua: open existingchar for non-admins too

--- scripts/namje_init.lua
+++ scripts/namje_init.lua
@@ -127,12 +127,8 @@ function init() ini()
         if not namje_byos.is_on_own_ship() then
             player.warp(player.ownShipWorldId())
         else
-            if namje_byos.is_fu() then
-                --something is closing the pane, so just dont put a source id
-                player.interact("scriptPane", "/interface/scripted/namje_existingchar/namje_existingchar.config")
-            else
-                player.interact("scriptPane", "/interface/scripted/namje_existingchar/namje_existingchar.config", player.id())
-            end
+            --something is closing the pane, so just dont put a source id
+            player.interact("scriptPane", "/interface/scripted/namje_existingchar/namje_existingchar.config")
         end
     end
 end
