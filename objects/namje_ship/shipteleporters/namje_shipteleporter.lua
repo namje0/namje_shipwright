0def624cbb20e1e62a29cbad5901fa481711dd63
namje0 <danguyen.11902@gmail.com>
2025-11-15 14:52:03 -0700

    refactor: remove print from namje_shipteleporter.lua

--- objects/namje_ship/shipteleporters/namje_shipteleporter.lua
+++ objects/namje_ship/shipteleporters/namje_shipteleporter.lua
@@ -2,10 +2,8 @@ require "/scripts/vec2.lua"
 local ini = init or function() end
 
 function init() ini()
-    sb.logInfo("namje === ship teleporter init")
     local id = world.id()
     if string.find(id, "ClientShipWorld") then
-        sb.logInfo("namje === ship teleporter found shipworld, setting pos of playerstart and namje_ship_spawn")
         world.setProperty("namje_ship_spawn", vec2.add(object.position(), {0, 1}))
         world.setPlayerStart(vec2.add(object.position(), {0, 1}))
     end
